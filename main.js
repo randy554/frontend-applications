f50d04bfb624799a21bfc99208c6b9730766daa2
randy554 <randy554@hotmail.com>
2020-11-23 23:40:55 +0100

    Added pulic folder

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+    Een Vue instance verbinden
+    met een html element
+ */
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'The best shop in The Netherlands'
+    }
+})
